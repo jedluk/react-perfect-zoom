8ad4c8bf94b7387cd82a0fc7882d260ca828a51b
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2020-01-02 21:19:21 +0100

    Higher Order components for touchEvents and mouseEvents

---
+++ src/components/hoc/withTouchEvents.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { INITIAL_POSITION } from '../../lib/rectangleCoordinates';
+import {
+  getRelativeCoordinates,
+  getPositions,
+  toggleBodyVisibility,
+  isOutsideImageRectangle
+} from './mixins';
+import { isNumber } from '../../lib/utils';
+
+const EXIT_MARGIN = 10;
+
+export default function withTouchEvents(Component) {
+  return class extends React.PureComponent {
+    constructor(props) {
+      super(props);
+      this.imageRef = React.createRef();
+      this.initialBodyOverflow = {};
+      this.thumbnailRect = {};
+      this.getPositions = getPositions.bind(this);
+      this.isOutsideImageRectangle = isOutsideImageRectangle(EXIT_MARGIN);
+      this.events = {
+        onTouchStart: this.handleTouchStart,
+        onTouchMove: this.handleTouchMove,
+        onTouchEnd: this.handleTouchEnd
+      };
+    }
+
+    state = {
+      startPosition: { ...INITIAL_POSITION },
+      currentPosition: { ...INITIAL_POSITION }
+    };
+
+    componentDidMount() {
+      const { overflowX, overflowY } = document.body.style;
+      this.initialBodyOverflow = { overflowX, overflowY };
+    }
+
+    handleTouchStart = (e) => {
+      document.documentElement.style.overflow = 'hidden';
+      this.thumbnailRect = this.imageRef.current.getBoundingClientRect();
+      const startPosition = getRelativeCoordinates(this.thumbnailRect, e.touches[0]);
+      this.setState({ startPosition });
+    };
+
+    handleTouchMove = (e) => {
+      if (!isNumber(this.state.startPosition.x)) {
+        return;
+      }
+      if (this.isOutsideImageRectangle(this.thumbnailRect)(e)) {
+        this.handleTouchEnd();
+      } else {
+        const currentPosition = getRelativeCoordinates(this.thumbnailRect, e.touches[0]);
+        this.setState({ currentPosition });
+      }
+    };
+
+    handleTouchEnd = () => {
+      document.documentElement.style.overflow = 'auto';
+      this.setState({
+        startPosition: { ...INITIAL_POSITION },
+        currentPosition: { ...INITIAL_POSITION }
+      });
+    };
+
+    render() {
+      const positions = this.getPositions();
+      return (
+        <Component
+          {...this.props}
+          positions={positions}
+          imageRef={this.imageRef}
+          events={this.events}
+        />
+      );
+    }
+  };
+}
