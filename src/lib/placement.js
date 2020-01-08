cc9308b1596221102e49023ca8b3d19994d14484
Jedrzej Lukasiuk <jedrzej.lukasiuk@gmail.com>
2020-01-08 20:58:41 +0100

    refactored placement module due to performance reasons

--- src/lib/placement.js
+++ src/lib/placement.js
@@ -16,89 +16,78 @@ export const getCroppedImageSize = (scale, { clickX, clickY, currentX, currentY 
 
 export const placementFunc = (placement) =>
   ({
-    right(image, positions, margin = 20) {
+    right({ margin, thumbnail }) {
       return {
         top: 0,
-        left: image.clientWidth + margin
+        left: thumbnail.clientWidth + margin
       };
     },
-    left(image, positions, margin = 20) {
+    left({ margin, croppedImage }) {
       return {
         top: 0,
-        left: Math.floor(
-          -margin -
-            getCroppedImageSize(image.naturalHeight / image.clientHeight, positions).width
-        )
+        left: Math.floor(-margin - croppedImage.width)
       };
     },
-    top(image, positions, margin = 20) {
+    top({ margin, croppedImage }) {
       return {
-        top: Math.floor(
-          -margin -
-            getCroppedImageSize(image.naturalHeight / image.clientHeight, positions)
-              .height
-        ),
+        top: Math.floor(-margin - croppedImage.height),
         left: 0
       };
     },
-    bottom(image, positions, margin = 20) {
+    bottom({ margin, thumbnail }) {
       return {
-        top: image.clientHeight + margin,
+        top: thumbnail.clientHeight + margin,
         left: 0
       };
     }
   }[placement]);
 
-export const withTranslation = (translate) => (obj) => ({
-  top: obj.top + getProperty(translate, 'y', 0),
-  left: obj.left + getProperty(translate, 'x', 0)
-});
-
-export const withAlignment = (align, { img, placement, positions }) => {
-  const alignVertical = ['left', 'right'].includes(placement);
-  const scale = img.naturalHeight / img.clientHeight;
-  const displacement = {
+export const calculateAlignment = ({ thumbnail, croppedImage, verticalAlign }) => (
+  align
+) =>
+  ({
     start: {
       top: 0,
       left: 0
     },
     center: {
-      top: alignVertical
-        ? Math.floor(
-            img.clientHeight / 2 - getCroppedImageSize(scale, positions).height / 2
-          )
+      top: verticalAlign
+        ? Math.floor(0.5 * thumbnail.clientHeight - 0.5 * croppedImage.height)
         : 0,
-      left: !alignVertical
-        ? Math.floor(
-            img.clientWidth / 2 - getCroppedImageSize(scale, positions).width / 2
-          )
+      left: !verticalAlign
+        ? Math.floor(0.5 * thumbnail.clientWidth - 0.5 * croppedImage.width)
         : 0
     },
     end: {
-      top: alignVertical
-        ? Math.floor(img.clientHeight - getCroppedImageSize(scale, positions).height)
-        : 0,
-      left: !alignVertical
-        ? Math.floor(img.clientWidth - getCroppedImageSize(scale, positions).width)
-        : 0
+      top: verticalAlign ? Math.floor(thumbnail.clientHeight - croppedImage.height) : 0,
+      left: !verticalAlign ? Math.floor(thumbnail.clientWidth - croppedImage.width) : 0
     }
-  }[align];
-  return (obj) => ({
-    top: obj.top + displacement.top,
-    left: obj.left + displacement.left
-  });
-};
+  }[align]);
+
+export const parseTranslation = (translate) => ({
+  top: getProperty(translate, 'y', 0),
+  left: getProperty(translate, 'x', 0)
+});
 
 export const getContainerPosition = ({
-  img,
   translate,
+  img: thumbnail,
   align = 'center',
   positions = {},
   margin = 20,
   placement = 'right'
 }) => {
-  const settledFunction = placementFunc(placement);
-  const initialPosition = settledFunction(img, positions, margin);
-  const translatedPosition = withTranslation(translate)(initialPosition);
-  return withAlignment(align, { img, placement, positions })(translatedPosition);
+  const scale = thumbnail.naturalHeight / thumbnail.clientHeight;
+  const croppedImage = getCroppedImageSize(scale, positions);
+  const verticalAlign = ['left', 'right'].includes(placement);
+  const basePositon = placementFunc(placement)({
+    thumbnail,
+    croppedImage,
+    margin
+  });
+  const translation = parseTranslation(translate);
+  const alignment = calculateAlignment({ croppedImage, thumbnail, verticalAlign })(align);
+  basePositon.top += translation.top + alignment.top;
+  basePositon.left += translation.left + alignment.left;
+  return basePositon;
 };
